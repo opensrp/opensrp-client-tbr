302b2fb72bfb518e5e2fe2b52ccba0dabc017285
Martin Ndegwa <ndegwamartin@gmail.com>
2017-10-09 18:12:24 +0300

    Add Home Activity
    Signed-off-by: Martin Ndegwa <ndegwamartin@gmail.com>

---
+++ opensrp-tbr/src/main/java/org/smartregister/tbr/BaseActivity.java
@@ -0,0 +1,42 @@
+package org.smartregister.tbr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by ndegwamartin on 09/10/2017.
+ */
+
+public class BaseActivity extends AppCompatActivity
+
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_language) {
+            return true;
+        } else if (id == R.id.action_logout) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ opensrp-tbr/src/main/java/org/smartregister/tbr/HomeActivity.java
@@ -0,0 +1,8 @@
+package org.smartregister.tbr;
+
+/**
+ * Created by ndegwamartin on 09/10/2017.
+ */
+
+public class HomeActivity extends BaseActivity {
+}
