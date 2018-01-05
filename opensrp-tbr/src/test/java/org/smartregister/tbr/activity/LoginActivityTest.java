d55a9ffaa5c878cdf777965763a43622c44a07f7
Martin Ndegwa <ndegwamartin@gmail.com>
2018-01-05 10:14:38 +0300

    Fix broken build
    Signed-off-by: Martin Ndegwa <ndegwamartin@gmail.com>

--- opensrp-tbr/src/test/java/org/smartregister/tbr/activity/LoginActivityTest.java
+++ opensrp-tbr/src/test/java/org/smartregister/tbr/activity/LoginActivityTest.java
@@ -12,7 +12,7 @@ import org.robolectric.android.controller.ActivityController;
 import org.smartregister.tbr.BaseUnitTest;
 import org.smartregister.tbr.event.ViewConfigurationSyncCompleteEvent;
 
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import static org.mockito.Mockito.times;
 import static org.powermock.api.mockito.PowerMockito.spy;
 
