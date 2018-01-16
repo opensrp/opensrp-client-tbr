e058b4e94daae4c30cf835d74ce661788a3ff962
Martin Ndegwa <ndegwamartin@gmail.com>
2018-01-16 18:35:53 +0300

    fix minor styling, update follow up button dynamic color change
    Signed-off-by: Martin Ndegwa <ndegwamartin@gmail.com>

--- opensrp-tbr/src/main/java/org/smartregister/tbr/helper/view/RenderPatientFollowupCardHelper.java
+++ opensrp-tbr/src/main/java/org/smartregister/tbr/helper/view/RenderPatientFollowupCardHelper.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import org.joda.time.DateTime;
+import org.joda.time.Days;
 import org.smartregister.tbr.R;
 import org.smartregister.tbr.repository.ResultDetailsRepository;
 import org.smartregister.tbr.util.Constants;
@@ -31,6 +34,17 @@ public class RenderPatientFollowupCardHelper extends BaseRenderHelper {
                 Button followUpView = (Button) view.findViewById(R.id.follow_up_button);
                 if (followUpView != null) {
                     followUpView.setText(context.getString(R.string.followup) + " - due " + Utils.formatDate(org.smartregister.util.Utils.toDate(patientDetails.get(Constants.KEY.NEXT_VISIT_DATE).toString(), true), "dd/MM"));
+                    DateTime treatmentStartDate = DateTime.parse(patientDetails.get(Constants.KEY.NEXT_VISIT_DATE).toString());
+                    int due = Days.daysBetween(new DateTime(), treatmentStartDate).getDays();
+                    if (due < 0)
+                        followUpView.setBackgroundResource(R.drawable.due_vaccine_red_bg);
+                    else if (due == 0) {
+                        followUpView.setBackgroundResource(R.drawable.due_vaccine_blue_bg);
+                    } else {
+                        ((TextView) view.findViewById(R.id.followup_text)).setTextColor(context.getResources().getColor(R.color.client_list_grey));
+                        followUpView.setBackgroundResource(R.drawable.due_vaccine_na_bg);
+                    }
+
 
                 }
             }
