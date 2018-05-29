d52ff40f981419833856eb1d05ecd63ca029ef90
Imran Arif <imran.arif@ihsinformatics.com>
2018-05-29 10:28:47 +0500

    IA: Some changes in advanced search results

--- opensrp-tbr/src/main/java/org/smartregister/tbr/fragment/AdvancedSearchResultsFragment.java
+++ opensrp-tbr/src/main/java/org/smartregister/tbr/fragment/AdvancedSearchResultsFragment.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -228,17 +229,20 @@ public class AdvancedSearchResultsFragment extends SecuredNativeSmartRegisterCur
         protected void onPostExecute(JSONObject resp) {
             try {
                 numResults.setText("" + getTotalClients(resp) + " result(s) found");
-                if (resp.has("presumptive")) {
+                if(resp == null){
+                    Snackbar.make(view,"Some error occurred",Snackbar.LENGTH_LONG).show();
+                }
+                if (resp != null && resp.has("presumptive")) {
                     preparePresumptiveRecycleView(resp);
                 } else {
                     showView(presumptiveNoResults);
                 }
-                if (resp.has("positive")) {
+                if (resp != null && resp.has("positive")) {
                     preparePositiveRecycleView(resp);
                 } else {
                     showView(positiveNoResults);
                 }
-                if (resp.has("intreatment")) {
+                if (resp != null && resp.has("intreatment")) {
                     prepareInTreatmentRecycleView(resp);
                 } else {
                     showView(intreatmentNoResults);
